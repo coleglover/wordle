f8f2c97d5dd65eec13d75bb7d8563e2221e9394a
coleglover <cole.r.glover@gmail.com>
2025-04-17 00:33:56 -0700

    adding submission logic, validation POST request, and input handling

--- wordle.js
+++ wordle.js
@@ -10,7 +10,10 @@
 // this function is generated when user clicks "start button"
 // function is define as async in order to use await
 
-let word;
+let secretWord;
+let puzzleNumber;
+let currentGuess = "";
+let currentRow = 0;
 
 async function generateWord() {
   try {
@@ -19,15 +22,10 @@ async function generateWord() {
     // .json is async; returns a promise
     const wordleData = await response.json();
 
-    word = wordleData.word;
-    const puzzleNumber = wordleData.puzzleNumber;
+    secretWord = wordleData.word;
+    puzzleNumber = wordleData.puzzleNumber;
 
-    console.log(`Puzzle number: ${puzzleNumber}\nWord: ${word}`);
-
-    document.querySelector(".puzzle-number").innerHTML =
-      "Puzzle number: " + puzzleNumber;
-    document.querySelector(".word-to-guess").innerHTML =
-      "Word of the day: " + word;
+    console.log(`Puzzle number: ${puzzleNumber}\nWord: ${secretWord}`);
   } catch (error) {
     console.log("Error fetching Wordle data:", error);
   }
@@ -36,8 +34,68 @@ async function generateWord() {
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
+
+async function isValidWord() {
+  console.log("validating word: ", currentGuess);
+  try {
+    const response = await fetch("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({
+        word: `${currentGuess}`,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    let validationData = await response.json();
+
+    console.log("> ", validationData.validWord);
+    return validationData.validWord;
+  } catch (error) {
+    console.log("Error fetching Wordle data:", error);
+  }
+}
+function updateTile() {
+  for (let i = 0; i < 5; i++) {
+    const tile = document.getElementById(`row-${currentRow}-col-${i}`);
+    tile.textContent = currentGuess[i] || "";
+  }
+}
+function submitGuess() {
+  if (currentGuess.length < 5) {
+    alert("Your guess must be 5 characters long!");
+  } else if (isValidWord()) {
+    currentRow++;
+    currentGuess = "";
+    // handle if word is not valid -- maybe move up in logic tree or add "!isValidWord()""
+  }
+}
+
+function validateGuess() {}
+
 document.addEventListener("keyup", (event) => {
   if (isLetter(event.key)) {
-    console.log(event.key);
+    if (currentGuess.length < 5) {
+      currentGuess += event.key;
+      console.log("current guess: ", currentGuess);
+      updateTile();
+    }
+  } else if (event.key === "Enter") {
+    submitGuess();
+    //validateGuess(currentGuess);
+  } else if (event.key === "Backspace") {
+    currentGuess = currentGuess.slice(0, -1);
+    updateTile();
+    console.log("current guess: ", currentGuess);
   }
 });
+
+function revealWord() {
+  document.querySelector(".puzzle-number").innerHTML =
+    "Puzzle number: " + puzzleNumber;
+  document.querySelector(".secret-word").innerHTML =
+    "Word of the day: " + secretWord;
+}
+
+generateWord();
